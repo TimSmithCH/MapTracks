8fe10d3a3becb2b20dceda87ae24e836c1fea9d8
Tim Smith <Tim.Smith@cern.ch>
2017-10-31 13:02:59 +0100

    Update deploy_prep.sh

--- test/deploy_prep.sh
+++ test/deploy_prep.sh
@@ -2,13 +2,9 @@
 set -e   # Exit with nonzero exit code if anything fails
 set -x   # Debug mode to echo commands
 
-pwd
-env
-ls -l
-which node
-npm config list
-npm config ls -l
 ls -lR tracks/
+git status
+git add tracks/geojson
 
 rm -r test
 rm -r node_modules
