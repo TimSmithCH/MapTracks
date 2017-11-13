44f4631c30486563589846a0644f27ab2bdabcef
TimSmith <Tim.Smith@cern.ch>
2017-11-13 23:51:23 +0100

    Flush all geojson files to production

--- test/deploy_prep.sh
+++ test/deploy_prep.sh
@@ -3,9 +3,12 @@ set -e   # Exit with nonzero exit code if anything fails
 set -x   # Debug mode to echo commands
 
 ls -lR tracks/
+rm -r test
+rm -r node_modules
+rm tracks/geojson/hike/*simple*
+rm tracks/geojson/bike/*simple*
+rm tracks/geojson/ski/*simple*
+
 git status
 git add tracks/geojson
 git commit -m "Converted more GeoJson files"
-
-rm -r test
-rm -r node_modules
