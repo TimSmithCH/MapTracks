9ed31d2d3314b931dfc1a459fbe43d49d5c53406
Tim Smith <tim.smith@cern.ch>
2017-11-16 13:09:45 +0100

    Keep simple files also

--- test/deploy_prep.sh
+++ test/deploy_prep.sh
@@ -2,12 +2,12 @@
 set -e   # Exit with nonzero exit code if anything fails
 set -x   # Debug mode to echo commands
 
-ls -lR tracks/
+#ls -lR tracks/
 rm -r test
 rm -r node_modules
-rm tracks/geojson/hike/*simple*
-rm tracks/geojson/bike/*simple*
-rm tracks/geojson/ski/*simple*
+#rm tracks/geojson/hike/*simple*
+#rm tracks/geojson/bike/*simple*
+#rm tracks/geojson/ski/*simple*
 
 git status
 git add tracks/geojson
