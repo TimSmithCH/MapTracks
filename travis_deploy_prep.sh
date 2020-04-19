2d30f1bbdf670a15546f7d965658eb4b7e61c9b9
Tim Smith <Tim.Smith@cern.ch>
2020-04-19 14:30:58 +0200

    Create travis_deploy_prep.sh

---
+++ travis_deploy_prep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e   # Exit with nonzero exit code if anything fails
+set -x   # Debug mode to echo commands
+
+#ls -lR tracks/
+rm -r test
+rm -r node_modules
+
+git status
+git add tracks/geojson
+git rm -r test
+git commit -m "Converted more GeoJson files"
