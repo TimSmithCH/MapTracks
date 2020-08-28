49739e2040da5abe595ddc5e4f7d93c5b65d8b3f
Tim Smith <tim.smith@cern.ch>
2020-08-28 23:24:45 +0200

    Action to deploy GitHub Pages

--- scripts/deploy_travis_prep.sh
+++ scripts/deploy_travis_prep.sh
@@ -3,11 +3,11 @@ set -e   # Exit with nonzero exit code if anything fails
 set -x   # Debug mode to echo commands
 
 #ls -lR
-rm -r node_modules
 
 git rm -rq tracks/3_gpx
 git commit -q -m "Dropped raw GPX files"
 
 git status
 git add tracks/2_geojson
+git add tracks/1_display
 git commit -m "Uploaded more GeoJSON files"
