f4e026ddeec4e843e78f9c5335120ebd92143ff9
Tim Smith <tim.smith@cern.ch>
2020-09-05 22:23:33 +0200

    Dent Favre

--- scripts/push_geojsons.sh
+++ scripts/push_geojsons.sh
@@ -3,11 +3,11 @@ set -e   # Exit with nonzero exit code if anything fails
 #set -x   # Debug mode to echo commands
 
 git status
-git add tracks/2_geojson
 git add tracks/1_display
+git add tracks/2_geojson
 git status
-echo "$commit_mess"
-MESS="Generate geoJSONs for: ${GITHUB_SHA}"
+MESS="${commit_mess} (GeoJSONs)"
+#MESS="Generate geoJSONs for: ${GITHUB_SHA}"
 echo "$MESS"
-#git commit -m "Uploaded more GeoJSON files"
-#git push
+git commit -m "$MESS"
+git push
