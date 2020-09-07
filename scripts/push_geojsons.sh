d3d432f398a995c5911e964eac1798e9d8b347ae
TimSmith <Tim.Smith@cern.ch>
2020-09-07 22:55:02 +0200

    Develop signal for tile job

--- scripts/push_geojsons.sh
+++ scripts/push_geojsons.sh
@@ -4,8 +4,18 @@ set -e   # Exit with nonzero exit code if anything fails
 
 git status
 git add tracks/1_display
+TILE=$(git status -s | grep -c -e "[ARC]  tracks/1_display")
+if [ $TILE ]; then
+  echo "none"
+  $tile_run=FALSE
+else
+  echo "some"
+  $tile_run=TRUE
+fi
+
 git add tracks/2_geojson
 git status
+
 MESS="${commit_mess} (GeoJSONs)"
 #MESS="Generate geoJSONs for: ${GITHUB_SHA}"
 echo "$MESS"
