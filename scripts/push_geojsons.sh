a227de398f8ebe689bb48ded8dd475c697021d0c
Tim Smith <tim.smith@cern.ch>
2020-09-10 22:05:59 +0200

    Another Baudichonne

--- scripts/push_geojsons.sh
+++ scripts/push_geojsons.sh
@@ -3,18 +3,19 @@
 set -x   # Debug mode to echo commands
 
 echo $tile_run
+git status -s
 git add tracks/1_display
 TILE=$(git status -s | grep -c -e "[ARC]  tracks/1_display")
 if [ $TILE ]; then
   echo "none"
-  $tile_run=FALSE
+  tile_run=0
 else
   echo "some"
-  $tile_run=TRUE
+  tile_run=1
 fi
 
 git add tracks/2_geojson
-git status
+git status -s
 
 MESS="${commit_mess} (GeoJSONs)"
 #MESS="Generate geoJSONs for: ${GITHUB_SHA}"
