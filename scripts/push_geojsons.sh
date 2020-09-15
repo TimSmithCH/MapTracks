5aef3327a4aa620b23e8dc9fbf4693f15a837836
Tim Smith <tim.smith@cern.ch>
2020-09-15 13:17:03 +0200

    Better tileset signalling

--- scripts/push_geojsons.sh
+++ scripts/push_geojsons.sh
@@ -3,15 +3,16 @@ set -e   # Exit with nonzero exit code if anything fails
 set -x   # Debug mode to echo commands
 
 git status -s
-if [[ -z $(git diff --quiet --exit-code tracks/1_display) ]]; then
-  echo "Tiles are good"
-  tile_run=0
-else
+#if ! git diff --quiet --exit-code tracks/1_display; then
+if ! git diff --quiet --exit-code tracks/3_gpx; then
   echo "Must update MapBox tileset"
   tile_run=1
+else
+  echo "Tiles are good"
+  tile_run=0
 fi
 
-#git add tracks/1_display
+git add tracks/1_display
 git add tracks/2_geojson
 git status -s
 
