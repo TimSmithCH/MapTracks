ba2d4d59252635cc6a003a2da8125d60b9303170
Tim Smith <tim.smith@cern.ch>
2020-09-15 10:47:39 +0200

    Tune geojson push

--- scripts/push_geojsons.sh
+++ scripts/push_geojsons.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-#set -e   # Exit with nonzero exit code if anything fails
+set -e   # Exit with nonzero exit code if anything fails
 set -x   # Debug mode to echo commands
 
-echo $tile_run
 git status -s
-git add tracks/1_display
-TILE=$(git status -s | grep -c -e "[ARC]  tracks/1_display")
-if [ $TILE ]; then
-  echo "none"
+if [[ -z $(git diff --quiet --exit-code tracks/1_display) ]]; then
+  echo "Tiles are good"
   tile_run=0
 else
-  echo "some"
+  echo "Must update MapBox tileset"
   tile_run=1
 fi
 
+#git add tracks/1_display
 git add tracks/2_geojson
 git status -s
 
 MESS="${commit_mess} (GeoJSONs)"
-#MESS="Generate geoJSONs for: ${GITHUB_SHA}"
 echo "$MESS"
-git commit -m "$MESS"
-git push
+#git commit -m "$MESS"
+#git push
+
+#########################
+#if [[ -z $(git status --untracked-files=no --porcelain tracks/1_display) ]]; then
