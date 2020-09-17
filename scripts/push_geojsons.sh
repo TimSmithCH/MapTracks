4ad65433c979613093fc33d5cd2112323ed1ba83
Tim Smith <tim.smith@cern.ch>
2020-09-17 13:29:49 +0200

    Remove all variables from signalling

--- scripts/push_geojsons.sh
+++ scripts/push_geojsons.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 set -e   # Exit with nonzero exit code if anything fails
-set -x   # Debug mode to echo commands
+#set -x   # Debug mode to echo commands
 
-git status -s
+# Update MapBox tileset?
 if ! git diff --quiet --exit-code tracks/1_display; then
-  echo "Must update MapBox tileset"
-  tile_run="Yes"
+  tupdate=1
 else
-  echo "Tiles are good"
-  tile_run="No"
+  tupdate=0
 fi
 
+# Push the modified GeoJSON files
 git add tracks/1_display
 git add tracks/2_geojson
 git status -s
-
 MESS="${commit_mess} (GeoJSONs)"
-echo "$MESS"
-git commit -m "$MESS"
-git push
+#git commit -m "$MESS"
+#git push
 
 #########################
 #if [[ -z $(git status --untracked-files=no --porcelain tracks/1_display) ]]; then
+exit tupdate
