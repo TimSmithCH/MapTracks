84143a9436349d38981a8274182b0c8df195014e
Tim Smith <tim.smith@cern.ch>
2020-09-17 14:46:11 +0200

    Signalling using exit codes

--- scripts/push_geojsons.sh
+++ scripts/push_geojsons.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e   # Exit with nonzero exit code if anything fails
+#set -e   # Exit with nonzero exit code if anything fails
 #set -x   # Debug mode to echo commands
 
 # Update MapBox tileset?
@@ -14,8 +14,8 @@ git add tracks/1_display
 git add tracks/2_geojson
 git status -s
 MESS="${commit_mess} (GeoJSONs)"
-#git commit -m "$MESS"
-#git push
+git commit -m "$MESS"
+git push
 
 #########################
 #if [[ -z $(git status --untracked-files=no --porcelain tracks/1_display) ]]; then
