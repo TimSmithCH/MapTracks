a55138357408fc5f84830d4c0c811c1b46e46661
TimSmith <Tim.Smith@cern.ch>
2020-09-24 20:12:40 +0200

    Integrating commute

--- scripts/deploy_tileset.sh
+++ scripts/deploy_tileset.sh
@@ -4,8 +4,8 @@ set -e   # Exit with nonzero exit code if anything fails
 
 TOKEN=$tileset_api
 
-TYPES=( bike hike run ski )
-FORCE=false
+TYPES=( bike hike run ski commute )
+FORCE="false"
 FRESHNESS=720
 
 # Catch-up with latest changes from first job
@@ -14,11 +14,11 @@ git pull --rebase=false
 # Update tileset sources when the summary GEOJsons have changed
 for type in "${TYPES[@]}"
 do
-  MODIFIED=false
+  MODIFIED="false"
   fileSUMM=tracks/1_display/${type}_tracks.geojson
   timeNOW="$(git log --pretty=format:%cd -n 1 --date=format:%s)"
   echo "Processing $fileSUMM"
-  if $FORCE ; then
+  if [ $FORCE -eq "true" ] ; then
     timeLAST=$timeNOW
   else
     timeLAST="$(git log --pretty=format:%cd -n 1 --date=format:%s -- $fileSUMM)"
@@ -26,7 +26,7 @@ do
   timeEL=$(($timeNOW-$timeLAST))
   if [[ "$timeEL" -le "$FRESHNESS" ]]; then
     printf "\n  Generating $type tileset\n"
-    MODIFIED=true
+    MODIFIED="true"
     #tilesets delete-source --token $TOKEN --force timsmithch ${type}_tracks
     #tilesets add-source --token $TOKEN timsmithch ${type}_tracks tracks/1_display/${type}_tracks.geojson
     tilesets upload-source --token $TOKEN timsmithch ${type}_tracks tracks/1_display/${type}_tracks.geojson --replace
@@ -34,14 +34,15 @@ do
     printf "\n  ."
   fi
 done
+printf "\n+++ Sources +++\n"
 tilesets list-sources --token $TOKEN timsmithch
 
 # Generate the new tileset
-if $MODIFIED ; then
+if [ $MODIFIED -eq "true" ; then
   # Create the initial tileset, only necessary first time, afterwards update is all
-  #tilesets create timsmithch.all_tracks --token $TOKEN --recipe ./all_tracks_recipe.json --name "All track types"
+  #tilesets create timsmithch.all_tracks --token $TOKEN --recipe scripts/all_tracks_recipe.json --name "All track types"
   # Update the recipe to generate the tilesets, only if it changes
-  #tilesets update-recipe --token $TOKEN timsmithch.all_tracks ./all_tracks_recipe.json
+  tilesets update-recipe --token $TOKEN timsmithch.all_tracks scripts/all_tracks_recipe.json
   # Launch the tilset generation and wait for completion
   tilesets publish --token $TOKEN timsmithch.all_tracks
   JOBRUN=true
