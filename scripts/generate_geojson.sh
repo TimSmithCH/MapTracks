e4139ff10cf95ab1ddd8f868ed8d2cdd7ee0cc6f
Tim Smith <tim.smith@cern.ch>
2022-09-18 20:55:38 +0200

    Jura

--- scripts/generate_geojson.sh
+++ scripts/generate_geojson.sh
@@ -49,6 +49,7 @@ do
       waypts=$(grep -c "<wpt" $fileIN||:)
       if [[ $waypts -gt 0 ]]
       then
+        printf "\n  Appending waypoints to GEOJSON: $fileOUT \n  "
         ogr2ogr --config GPX_ELE_AS_25D YES -nln LayerName -lco COORDINATE_PRECISION=7 -f GeoJSON -append -update $fileOUT $fileIN waypoints
       fi
       python scripts/normalise_geojson.py -file $fileOUT
