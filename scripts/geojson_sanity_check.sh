8b194742d73fd9195767f703f1c186af34b8b6dc
Tim Smith <tim.smith@cern.ch>
2022-10-02 20:31:07 +0200

    Haralds

---
+++ scripts/geojson_sanity_check.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e   # Exit with nonzero exit code if anything fails
+#set -x   # Debug mode to echo commands
+
+TYPES="bike hike run ski wip commute vehicle"
+
+printf "\n=== Forward Check ===\n  "
+for type in $TYPES
+do
+  printf "\n=== Processing $type files ===\n  "
+  FILES=tracks/3_gpx/$type/*.gpx
+  for f in $FILES
+  do
+    file=${f##*/}
+    base=${file%.gpx}
+    fileIN=tracks/3_gpx/$type/$base.gpx
+    fileOUT=tracks/2_geojson/$type/$base.geojson
+    if [[ ! -f $fileOUT ]] ; then
+      printf " $fileOUT not found\n"
+    else
+      timeIN="$(git log --pretty=format:%cd -n 1 --date=unix -- $fileIN)"
+      timeOUT="$(git log --pretty=format:%cd -n 1 --date=unix -- $fileOUT)"
+      if [[ $timeIN -gt $timeOUT ]]; then
+        printf " $fileOUT too old\n"
+      fi
+    fi
+  done
+done
+
+printf "\n=== Reverse Check ===\n  "
+for type in $TYPES
+do
+  printf "\n=== Processing $type files ===\n  "
+  FILES=tracks/2_geojson/$type/*.geojson
+  for f in $FILES
+  do
+    file=${f##*/}
+    base=${file%.geojson}
+    fileOUT=tracks/2_geojson/$type/$base.geojson
+    fileIN=tracks/3_gpx/$type/$base.gpx
+    if [[ ! -f $fileIN ]] ; then
+      printf " $fileOUT is extra\n"
+    fi
+  done
+done
+
+printf "\n=== End of Check ===\n  "
