6a4b006f15adb802d728ef2f6447e75c3ec1a8af
Tim Smith <tim.smith@cern.ch>
2023-08-02 13:08:42 +0200

    Make action cronable

--- scripts/PullStravaData.py
+++ scripts/PullStravaData.py
@@ -2,22 +2,29 @@
 import requests
 import json
 import time
+import datetime
 import os
+import re
+import gpxpy
 
-activitiesFile = "features/LastStravaIDRead.json"
+historyFile = "features/LastStravaIDRead.json"
+tokenFile = "token.json"
 
 def init():
     global tokens
     global stravaData
-    # Construct an expired token, to force immediate refresh (hence tokens dont need to be valid)
-    tokens = {
-        "token_type": "Bearer",
-        "access_token": "4444444444444444444444444444444444444444",
-        "expires_at": 1690747915,
-        "expires_in": 0,
-        "refresh_token": "6666666666666666666666666666666666666666"
-    }
-    stravaData = json.load(open(activitiesFile))
+    if os.path.isfile(tokenFile):
+        tokens = json.load(open(tokenFile))
+    else:
+        # Construct an expired token, to force immediate refresh (hence tokens dont need to be valid)
+        tokens = {
+            "token_type": "Bearer",
+            "access_token": "987654321",
+            "expires_at": 1690747915,
+            "expires_in": 0,
+            "refresh_token": "123456789"
+        }
+    stravaData = json.load(open(historyFile))
 
 def refreshTokens():
     body = {
@@ -34,7 +41,7 @@ def refreshTokens():
     new_tokens = json.loads(resp.text)
     return new_tokens
 
-def getAccessToken():
+def getAccessToken(quietly):
     global tokens
     if tokens.get("expires_at") < (time.time()):
         # REFRESH TOKENS
@@ -44,14 +51,16 @@ def getAccessToken():
             print("ERROR: Could not refresh tokens")
             return None
         print("New Token: " + tokenResponse["access_token"])
+        json.dump(tokenResponse, open(tokenFile, mode="w"))
         tokens = tokenResponse
         return tokens["access_token"]
     else:
-        print("Using Token: " + tokens["access_token"])
+        if not quietly:
+            print("Using Token: " + tokens["access_token"])
         return tokens["access_token"]
 
 def fetchActivities(page, per_page=100):
-    accessToken = getAccessToken()
+    accessToken = getAccessToken(False)
     if(not accessToken):
         print("ERROR: No access token - cant request activities")
         return {}
@@ -60,6 +69,20 @@ def fetchActivities(page, per_page=100):
         'authorization': "Bearer " + accessToken
     }
     resp = requests.get("https://www.strava.com/api/v3/athlete/activities?page="+str(page)+"&per_page="+str(per_page), headers=headers)
+    fetched = json.loads(resp.text)
+    return fetched
+
+def fetchActivityStream(actid):
+    accessToken = getAccessToken(True)
+    if(not accessToken):
+        print("ERROR: No access token - cant request activity stream")
+        return {}
+    headers = {
+        'accept': 'application/json',
+        'authorization': "Bearer " + accessToken
+    }
+    keys = "time,latlng,altitude"
+    resp = requests.get("https://www.strava.com/api/v3/activities/"+str(actid)+"/streams?keys="+keys+"&key_by_type=True", headers=headers)
     fetched = json.loads(resp.text)
     return fetched
 
@@ -88,13 +111,46 @@ def updateActivitiesList():
         print(" - "+str(i["id"])+" : "+str(i["type"])+" : "+str(i["name"]))
     return activitiesToAdd
 
+def createGPXFile(activity_name,activity_id,activity_start,activity_sport,stream):
+    gpx = gpxpy.gpx.GPX()
+    dt = datetime.datetime.fromisoformat(activity_start.replace('Z', '+00:00'))
+    gpx.time = dt
+    gpx.name = str(activity_name)
+    gpx.keywords = str(activity_id)
+    # Create track in GPX:
+    gpx_track = gpxpy.gpx.GPXTrack()
+    gpx_track.name = activity_name
+    gpx_track.comment = str(dt.date())
+    gpx_track.type = activity_sport
+    gpx_track.description = '1.0'
+    gpx.tracks.append(gpx_track)
+    # Create segment in GPX track:
+    gpx_segment = gpxpy.gpx.GPXTrackSegment()
+    gpx_track.segments.append(gpx_segment)
+    # Create points:
+    for i,p in enumerate(stream['latlng']['data']):
+        trkpt_dt = dt + datetime.timedelta(seconds=int(stream['time']['data'][i]))
+        gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(p[0], p[1], elevation=stream['altitude']['data'][i], time=trkpt_dt))
+    return gpx
+
+
 
 if __name__ == "__main__":
 
     activities = updateActivitiesList()
-    currentSport = list(filter(lambda obj: not obj['commute'], activities))
+    #filteredActivities = list(filter(lambda obj: not obj['commute'], activities))
+    filteredActivities = list(filter(lambda obj: obj['type'] == "Hike", activities))
     print("Non-commutes retained:")
-    for i in currentSport:
-        print("ID: {}   Type {} ({})   Name: {}".format(i["id"],i["type"],i["sport_type"],i["name"]))
-    dates = list(map(lambda obj: obj['start_date'], currentSport))
-    #dates_conv = dt.date2num(dates)
+    for i in filteredActivities:
+        print("ID: {}   Type {} ({})   Name: {}".format(i["id"],i["type"],i["sport_type"],i["name"],i["start_date"],i["name"]))
+        stream = fetchActivityStream(i["id"])
+        if not stream['latlng']['resolution'] == "high":
+            print("WARN: Stream truncated ({}) from {} to {}".format(stream['latlng']['resolution'],stream['latlng']['original_size'],len(stream['latlng']['data'])))
+        s = str(i["id"])+"."+str(i["name"])
+        s = re.sub(r"-", ' ', s)
+        activity_name = re.sub(r"\s+", '_', s)
+        gpx = createGPXFile(activity_name,i["id"],i["start_date"],i["sport_type"],stream)
+        #print('Created GPX:', gpx.to_xml())
+        outfile = activity_name+".gpx"
+        with open(outfile, "w") as f:
+            f.write(gpx.to_xml())
