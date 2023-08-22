51eb419d5ae0f81d631b507f502b8abe1a997e1b
Tim Smith <tim.smith@cern.ch>
2023-08-22 17:10:29 +0200

    Making generation more specific

--- scripts/generate_geojson.sh
+++ scripts/generate_geojson.sh
@@ -1,16 +1,51 @@
 #!/bin/bash
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+#
+# DESCRIPTION
+#    Generate GeoJSON when GPX file is new, modified or newer that GEOJSON file
+#
+# EXAMPLES
+#    ./generate_geojson.sh -s hike   ## Limit to only one sport directory to scan
+#    ./generate_geojson.sh -f true   ## Force the regeneration if if not new
+#
+# IMPLEMENTATION
+#    Author       Tim Smith
+#    Copyright    Copyright (c) Tim Smith
+#    Licence      GNU General Public License
+#
+#-------------------------------------------------------------------------------
 set -e   # Exit with nonzero exit code if anything fails
 #set -x   # Debug mode to echo commands
+
+while getopts :s:f:d: opt
+do
+    case "${opt}" in
+        s ) SPORTS=${OPTARG};;
+        f ) FORCE=${OPTARG};;
+        d ) DRYRUN=${OPTARG};;
+        \? ) echo "Usage: cmd [-s sports] [-f force] [-d dryrun]"
+    esac
+done
+
+# Defaults set if arguments aren't passed
+if [ -z "$SPORTS" ]; then
+  SPORTS=( bike hike run ski skiclimb swim wip commute vehicle )
+fi
+if [ -z "$FORCE" ]; then
+  FORCE="false"
+fi
+if [ -z "$DRYRUN" ]; then
+  DRYRUN="false"
+fi
+
 
 # WIP directory disappears when empty in GitHub, so continually check it is there
 mkdir -pv tracks/2_geojson/wip
 mkdir -pv tracks/2_geojson/vehicle
 #mkdir -pv tracks/2_geojson/commute
 
-TYPES="bike hike run ski skiclimb swim wip commute vehicle"
-FORCE="FALSE"
-
-for type in $TYPES
+for type in $SPORTS
 do
   printf "\n=== Processing $type files ===\n  "
   MODIFIED="false"
@@ -43,16 +78,28 @@ do
       # simplify 0.000025 tolerance = resolution of 2m
       # GPX_ELE_AS_25D allows GPX elevation data to be read in and passed out to GeoJSON as 2.5D (ie 3D where elevation unused in rendering)
       # explodecollections creates a feature per track segment
-      ogr2ogr --config GPX_ELE_AS_25D YES -nlt LINESTRING -simplify 0.00002 -nln LayerName -lco COORDINATE_PRECISION=7 -explodecollections -f GeoJSON $fileOUT $fileIN tracks
+      if [[ "$DRYRUN" == "false" ]] ; then
+        ogr2ogr --config GPX_ELE_AS_25D YES -nlt LINESTRING -simplify 0.00002 -nln LayerName -lco COORDINATE_PRECISION=7 -explodecollections -f GeoJSON $fileOUT $fileIN tracks
+      else
+        echo "ogr2ogr (options) -f GeoJSON $fileOUT $fileIN tracks"
+      fi
       # Avoid grep stopping script on null match with "pipe true" ||: trick
       # grep has exit code 1 for null match and this is usually an error status, but the pipe ensures the last command is asuccessful true
       waypts=$(grep -c "<wpt" $fileIN||:)
       if [[ $waypts -gt 0 ]]
       then
         printf "\n  Appending waypoints to GEOJSON: $fileOUT \n  "
-        ogr2ogr --config GPX_ELE_AS_25D YES -nln LayerName -lco COORDINATE_PRECISION=7 -f GeoJSON -append -update $fileOUT $fileIN waypoints
+        if [[ "$DRYRUN" == "false" ]] ; then
+          ogr2ogr --config GPX_ELE_AS_25D YES -nln LayerName -lco COORDINATE_PRECISION=7 -f GeoJSON -append -update $fileOUT $fileIN waypoints
+        else
+          echo "ogr2ogr (options) -f GeoJSON -append -update $fileOUT $fileIN waypoints"
+        fi
       fi
-      python scripts/geojson_normalise.py -file $fileOUT
+      if [[ "$DRYRUN" == "false" ]] ; then
+        python scripts/geojson_normalise.py -file $fileOUT
+      else
+        echo "python scripts/geojson_normalise.py -file $fileOUT"
+      fi
     else
       printf "."
     fi
@@ -61,6 +108,10 @@ do
   fileSUMM=tracks/1_display/${type}_tracks.geojson
   if [[ "$MODIFIED" == "true" || ! -f $fileSUMM ]] ; then
     printf "\n+++ Generating aggregate: $fileSUMM +++\n"
-    python scripts/geojsons_merge.py -d tracks/2_geojson/$type -o $fileSUMM
+    if [[ "$DRYRUN" == "false" ]] ; then
+      python scripts/geojsons_merge.py -d tracks/2_geojson/$type -o $fileSUMM
+    else
+      echo "python scripts/geojsons_merge.py -d tracks/2_geojson/$type -o $fileSUMM"
+    fi
   fi
 done
